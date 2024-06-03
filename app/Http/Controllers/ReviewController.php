fa9da9b8e20772396ac189bb69e4d524d680ff9c
unknown <mohmmadalean1@gmaile.com>
2024-06-04 00:16:20 +0300

    update

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class ReviewController extends Controller
 {
-    //
+    public function index()
+    {
+        $reviews = Review::paginate(10);
+        return response()->json($reviews);
+    }
+
+    public function business_review($id)
+    {
+        $reviews = Review::where('business_id', $id)
+            ->paginate(10);
+        if (count($reviews) === 0)
+            return response()->json(abort(404));
+
+        return response()->json($reviews);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'business_id' => 'required',
+            'review' => 'required|string',
+            'stars' => 'required',
+        ]);
+
+        if ($validator->fails())
+            return \response()->json($validator->errors()->toJson());
+
+        $review = new Review();
+        $review->user_id = Auth::id();
+        $review->business_id = $request->business_id;
+        $review->review = $request->review;
+        $review->stars = $request->stars;
+        $review->save();
+
+        return response()->json("Review is added");
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'review' => 'required|string',
+            'stars' => 'required',
+        ]);
+
+        if ($validator->fails())
+            return \response()->json($validator->errors()->toJson());
+
+        $review =  Review::findOrFail($id);
+        $review->review = $request->review;
+        $review->stars = $request->stars;
+        $review->save();
+
+        return response()->json("Review is updated");
+    }
+
+    public function destroy($id)
+    {
+        $review = Review::findOrFail($id);
+        $review->delete();
+        return response()->json('Review is deleted');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\Business\ServiceCotroller;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +18,14 @@ Route::apiResource('/business', BusinessController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('/service', ServiceCotroller::class);
-    Route::apiResource('/booking', BookingController::class);
     Route::post('/service/update/{id}', [ServiceCotroller::class, 'update']);
+
+    Route::apiResource('/booking', BookingController::class);
     Route::post('/booking/update/{id}', [BookingController::class, 'update']);
+
+    Route::apiResource('/review', ReviewController::class);
+    Route::post('/review/update/{id}', [ReviewController::class, 'update']);
+    Route::get('/business_review/{id}', [ReviewController::class, 'business_review']);
 });
 
 Route::post('/business/update/{id}', [BusinessController::class, 'update']);
